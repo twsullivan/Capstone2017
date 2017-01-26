50231234020b4761deeeebb513081f3cc1472093
Lance Fisher <ldf8@students.uwf.edu>
2017-01-25 20:01:07 -0600

    Added skeleton eclipse project files

---
+++ Devel_Steelers/src/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author lance
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("TEST");
+
+	}
+
+}
