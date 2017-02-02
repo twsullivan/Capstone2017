2ae175da15211150ce612960e46ac7fbd2f3d2da
Lance Fisher <ldf8@students.uwf.edu>
2017-02-01 19:48:50 -0600

    added scan constructor and robustness to argument parsing

---
+++ Devel_Steelers/src/Scan.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ */
+
+/**
+ * @author lance
+ *
+ */
+public class Scan {
+	private static String inputFile = "";
+	private static String outputFile = "";
+	private static String environment = "";
+	private static String environmentIP = "";
+	private static String name = "";
+	private static int queriesPerSecond = 0;
+	private static boolean quiet = false;
+
+	/**
+	 * 
+	 */
+	public Scan(String[] inputArgs) throws Exception {
+
+		for (int i = 0; i < inputArgs.length; i += 2)
+			if (inputArgs[i] == "-i")
+				setInputFile(inputArgs[i + 1]);
+			else if (inputArgs[i] == "-o")
+				setOutputFile(inputArgs[i + 1]);
+			else if (inputArgs[i] == "-e") {
+				setEnvironment(inputArgs[i + 1].split(":")[0]);
+				setEnvironmentIP(inputArgs[i + 1].split(":")[1]);
+			} else if (inputArgs[i] == "-t")
+				setQueriesPerSecond(Integer.parseInt(inputArgs[i + 1]));
+			else if (inputArgs[i] == "-n")
+				setName(inputArgs[i + 1]);
+			else if (inputArgs[i] == "-q") {
+				setQuiet(true);
+				i--;
+			} else
+				throw new Exception("Invalid arguments provided (duplicate or unknown).");
+
+		if (getInputFile().isEmpty() || getOutputFile().isEmpty() || getEnvironment().isEmpty() || getName().isEmpty()
+				|| getEnvironmentIP().isEmpty() || getQueriesPerSecond() < 1)
+			throw new Exception("Not enough arguments.");
+	}
+
+	/**
+	 * @return the inputFile
+	 */
+	public static String getInputFile() {
+		return inputFile;
+	}
+
+	/**
+	 * @param inputFile
+	 *            the inputFile to set
+	 */
+	private static void setInputFile(String inputFile) {
+		Scan.inputFile = inputFile;
+	}
+
+	/**
+	 * @return the outputFile
+	 */
+	public static String getOutputFile() {
+		return outputFile;
+	}
+
+	/**
+	 * @param outputFile
+	 *            the outputFile to set
+	 */
+	private static void setOutputFile(String outputFile) {
+		Scan.outputFile = outputFile;
+	}
+
+	/**
+	 * @return the environment
+	 */
+	public static String getEnvironment() {
+		return environment;
+	}
+
+	/**
+	 * @param environment
+	 *            the environment to set
+	 */
+	private static void setEnvironment(String environment) {
+		Scan.environment = environment;
+	}
+
+	/**
+	 * @return the environmentIP
+	 */
+	public static String getEnvironmentIP() {
+		return environmentIP;
+	}
+
+	/**
+	 * @param environmentIP
+	 *            the environmentIP to set
+	 */
+	private static void setEnvironmentIP(String environmentIP) {
+		Scan.environmentIP = environmentIP;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public static String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	private static void setName(String name) {
+		Scan.name = name;
+	}
+
+	/**
+	 * @return the queriesPerSecond
+	 */
+	public static int getQueriesPerSecond() {
+		return queriesPerSecond;
+	}
+
+	/**
+	 * @param queriesPerSecond
+	 *            the queriesPerSecond to set
+	 */
+	private static void setQueriesPerSecond(int queriesPerSecond) {
+		Scan.queriesPerSecond = queriesPerSecond;
+	}
+
+	/**
+	 * @return the quiet
+	 */
+	public static boolean isQuiet() {
+		return quiet;
+	}
+
+	/**
+	 * @param quiet
+	 *            the quiet to set
+	 */
+	private static void setQuiet(boolean quiet) {
+		Scan.quiet = quiet;
+	}
+
+}
--- Devel_Steelers/src/main.java
+++ Devel_Steelers/src/main.java
@@ -8,129 +8,32 @@
  */
 public class main {
 
-	private static String inputFile = "";
-	private static String outputFile = "";
-	private static String environment = "";
-	private static int queriesPerSecond = 0;
-	private static boolean[] optionsCheck = { false, false, false, false };
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		String[] testArgs = { "-i", "input.json", "-o", "output.json", "-e", "8.8.8.8", "-t", "100" };
-		testInputArgs(testArgs);
-
-	}
-
-	private static void testInputArgs(String[] testArgs) {
-		for (int i = 0; i < testArgs.length; i += 2) {
-			if (testArgs[i] == "-i") {
-				setInputFile(testArgs[i + 1]);
-				setOptionsCheck(0);
-			} else if (testArgs[i] == "-o") {
-				setOutputFile(testArgs[i + 1]);
-				setOptionsCheck(1);
-			} else if (testArgs[i] == "-e") {
-				setEnvironment(testArgs[i + 1]);
-				setOptionsCheck(2);
-			} else if (testArgs[i] == "-t") {
-				setQueriesPerSecond(Integer.parseInt(testArgs[i + 1]));
-				setOptionsCheck(3);
-			} else
-				usageMessage();
-
+		String[] testArgs = { "-i", "input.json", "-o", "output.json", "-q", "-e", "TEST:8.8.8.8", "-t", "50", "-n",
+				"John Smith" };
+		try{
+			Scan scn = new Scan(testArgs);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			usageMessage();
 		}
-		for (int i = 0; i < 4; i++)
-			if (getOptionsCheck()[i] == false)
-				usageMessage();
-
+		
 	}
 
 	private static void usageMessage() {
 
 		System.out.println(
-				"USAGE:  java -jar queryDNS.jar -i <domainNames> -o <results> -e <environment> -t <queriesPerSecond>");
+				"USAGE:  java -jar queryDNS.jar -i <domainNames> -o <results> -e <environment> -t <queriesPerSecond> <-q>");
 		System.out.println("-i\t inputFile - The file containing the domain name list.");
 		System.out.println("-o\t outputFile - The file containing the results of the queries.");
 		System.out.println("-e\t environment - The DNS server that will be queried.");
 		System.out.println("-t\t queriesPerSecond - Max queries per second.");
+		System.out.println("-q\t quietFlag - Disables message regarding remaining queries.");
 		System.exit(0);
-	}
-
-	/**
-	 * @return the inputFile
-	 */
-	private static String getInputFile() {
-		return inputFile;
-	}
-
-	/**
-	 * @param inputFile
-	 *            the inputFile to set
-	 */
-	private static void setInputFile(String inputFile) {
-		main.inputFile = inputFile;
-	}
-
-	/**
-	 * @return the outputFile
-	 */
-	private static String getOutputFile() {
-		return outputFile;
-	}
-
-	/**
-	 * @param outputFile
-	 *            the outputFile to set
-	 */
-	private static void setOutputFile(String outputFile) {
-		main.outputFile = outputFile;
-	}
-
-	/**
-	 * @return the environment
-	 */
-	private static String getEnvironment() {
-		return environment;
-	}
-
-	/**
-	 * @param environment
-	 *            the environment to set
-	 */
-	private static void setEnvironment(String environment) {
-		main.environment = environment;
-	}
-
-	/**
-	 * @return the queriesPerSecond
-	 */
-	private static int getQueriesPerSecond() {
-		return queriesPerSecond;
-	}
-
-	/**
-	 * @param queriesPerSecond
-	 *            the queriesPerSecond to set
-	 */
-	private static void setQueriesPerSecond(int queriesPerSecond) {
-		main.queriesPerSecond = queriesPerSecond;
-	}
-
-	/**
-	 * @return the optionsCheck
-	 */
-	private static boolean[] getOptionsCheck() {
-		return optionsCheck;
-	}
-
-	/**
-	 * @param optionsCheck
-	 *            the optionsCheck to set
-	 */
-	private static void setOptionsCheck(int option) {
-		main.optionsCheck[option] = true;
 	}
 }
