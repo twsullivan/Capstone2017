65bcda4ee9452cb5fcac687549ce4ec4e6212643
codyman489 <codyman489@yahoo.com>
2017-02-07 13:18:49 -0600

    Turned JGet into a class, with an additional TesterClass

---
+++ Devel_Ravens/JGet/src/TesterJGet.java
@@ -0,0 +1,14 @@
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class TesterJGet {
+    public static void main(String[] args) throws IOException{
+        JGet g = new JGet();
+    }
+}
