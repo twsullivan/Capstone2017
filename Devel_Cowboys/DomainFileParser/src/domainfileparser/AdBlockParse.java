2609435fe352be04dc696b2c139531817c6a91d0
Zack McColgan <zam3@students.uwf.edu>
2017-02-21 20:31:15 -0600

    AdBlockDNSList Functionality
    Changes:
    1) Added AdBlockDNSList functionality to project
    2) Added two example DNS lists used to test my class
    3) Deleted unneeded test.txt file

---
+++ Devel_Cowboys/DomainFileParser/src/domainfileparser/AdBlockParse.java
@@ -0,0 +1,44 @@
+package domainfileparser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Parses an Adblock text file array, returns a clean array of DNS's
+ *
+ * @author Zack McColgan
+ */
+public class AdBlockParse {
+
+    public AdBlockParse() 
+    {
+
+    }
+
+    public static String[] parse(String[] lines) 
+    {
+        if (lines.length < 1) // No lines to parse
+        {
+            System.exit(666);
+        }
+
+        List<String> lineList = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) 
+        {
+            if (lines[i].charAt(0) == '!' || lines[i].charAt(0) == '[') 
+            {
+                //Skip line
+            } 
+            else if(lines[i].charAt(0) == '|') 
+            {
+                lineList.add(lines[i].substring(2, lines[i].length() - 1));
+            } 
+            else 
+            {
+               lineList.add(lines[i]); 
+            }
+        }
+        return lineList.toArray(new String[0]);
+    }
+}
