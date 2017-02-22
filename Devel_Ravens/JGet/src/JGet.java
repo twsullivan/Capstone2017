dfa05d23892d5738da647fbef174349fe2ca79a1
codyman489 <codyman489@yahoo.com>
2017-02-21 21:45:49 -0600

    Updated JGet for better coding flow

--- Devel_Ravens/JGet/src/JGet.java
+++ Devel_Ravens/JGet/src/JGet.java
@@ -11,16 +11,37 @@
 
 import java.io.*;
 import java.net.*;
+//import java.util.ArrayList;
 
 public class JGet {
+    private String url;
+    private String outFileName;
+    //private ArrayList<String> inputs;
+    private File outFile;
+    private FileWriter fWriter;
+    private PrintWriter pWriter;
     
-    public JGet() throws IOException{
-        JGet g = new JGet("www.uwf.edu");
+    public JGet(){
+        url = "";
+        outFileName = "HTMLDump.txt";
     }
-    
     public JGet(String str) throws IOException{
-        //Building the String for parsing.
-        String url = "";
+        super();
+        runJGet(str);
+    }
+    private void runJGet(String str) throws IOException{
+        //Variables the website's datastreams.
+        URL u;
+        InputStream is;
+        DataInputStream dis;
+        String site;
+        
+        //Create output file.
+        outFile = new File (outFileName);
+        fWriter = new FileWriter (outFile, true);
+        pWriter = new PrintWriter (fWriter);
+
+        //Building the string for switch case.
         String tmp = str.charAt(0) + "" + str.charAt(1) 
                 + "" + str.charAt(2) + "";
         
@@ -37,26 +58,15 @@ public class JGet {
                 //System.out.println("default");
                 break;
         }
-    
-        //Create output file.
-        File outFile = new File ("HTMLDump.txt");
-        FileWriter fWriter = new FileWriter (outFile, true);
-        PrintWriter pWriter = new PrintWriter (fWriter);
-    
-        //Opening datastreams for HTML Dump.
-        URL u;
-        InputStream is = null;
-        DataInputStream dis;
-        String s;
-
+        
         try{
             u = new URL(url);
             is = u.openStream();
             dis = new DataInputStream(new BufferedInputStream(is));
             
             //Reading in the datastream.
-            while ((s = dis.readLine()) != null){
-                pWriter.println(s);
+            while ((site = dis.readLine()) != null){
+                pWriter.println(site);
             }
             
             pWriter.println("------------ END OF DUMP ----------------------------");
@@ -68,17 +78,24 @@ public class JGet {
         }
         catch (MalformedURLException mue){
             System.err.println("Ouch - a MalformedURLException happened.");
-            mue.printStackTrace();
             System.exit(2);
         }
         catch (IOException ioe){
             System.err.println("Oops- an IOException happened.");
-            ioe.printStackTrace();
             System.exit(3);
         }
         finally{
             System.out.println(url + " HTML code has been added to the dump file.");
         }
-        
+    }
+    
+    //Getters and Setters
+    public String getOutput(){return outFileName;}
+    public void setOutput(String outFileName){
+        this.outFileName = outFileName;
+    }
+    public String getURL(){return url;}
+    public void setURL(String url){
+        this.url = url;
     }
 }
