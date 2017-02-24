9f5920fc1ec526c626b0019f64bfbf6d5f0a083a
auhsoj1993 <jkm19@students.uwf.edu>
2017-02-23 22:14:08 -0600

    Added initial DNSMessageHeaderFlagOpcodes file

---
+++ Devel_Falcons/src/DNSQueryTool/DNSMessage/DNSMessageHeaderFlagOpcodes.java
@@ -0,0 +1,10 @@
+package DNSQueryTool.DNSMessage;
+
+public class DNSMessageHeaderFlagOpcodes {
+    public static final int QUERY = 0;
+    public static final int IQUERY = 1;
+    public static final int STATUS = 2;
+    public static final int RESERVED = 3;
+    public static final int NOTIFY = 4;
+    public static final int UPDATE = 5;
+}
