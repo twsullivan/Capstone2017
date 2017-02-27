fd323b404549d0a4efcf1c4ee20d15d70314c4a7
dloucks01 <dloucks01@gmail.com>
2017-02-26 20:53:52 -0600

    added question

---
+++ Devel_Falcons/src/DNSQueryTool/DNSMessage/DNSMessageQuestionClass.java
@@ -0,0 +1,10 @@
+//Denny Loucks
+//Capstone
+//DNSQuery
+
+package DNSQueryTool.DNSMessage;
+
+public class DNSMessageQuestionClass {
+    public static int IN = 1;
+    public static int ANY = 255;
+}
