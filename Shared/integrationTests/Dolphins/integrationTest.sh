8ae56caaef885c00d9a63f6308bf6f21f866f1a2
Alberto Medeiros <acm52@students.uwf.edu>
2017-03-27 14:14:07 -0500

    Fixed integration test. Works, uses temporary directory for now to run analysis on.

--- Shared/integrationTests/Dolphins/integrationTest.sh
+++ Shared/integrationTests/Dolphins/integrationTest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #This will be the path to the centralized output files from the Query tools.
-PATH_TO_FILES="../Shared/OutputJsonFiles/OrSomething"
+PATH_TO_FILES="../../../Devel_Dolphins/jsonTestFiles"
+#Using a temporary directory in our folder for now, which holds some test JSON files.
 
 #Goes up 3 directorys, which should place it on 201701_Capstone
-java -jar ../../../Devel_Dolphins/Build/JAnalysis.jar &{PATH_TO_FILES}
+java -jar ../../../Devel_Dolphins/Build/JAnalysis.jar ${PATH_TO_FILES}
