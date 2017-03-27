d159a1ecf2ce67394a8fcdbcb334b330bd09d817
Alberto Medeiros <acm52@students.uwf.edu>
2017-03-27 14:42:14 -0500

    integration script now outputs results to a timestamped file.

--- Shared/integrationTests/Dolphins/integrationTest.sh
+++ Shared/integrationTests/Dolphins/integrationTest.sh
@@ -3,5 +3,7 @@
 PATH_TO_FILES="../../../Devel_Dolphins/jsonTestFiles"
 #Using a temporary directory in our folder for now, which holds some test JSON files.
 
+timestamp=$( date +%m%d%H%M )
+
 #Goes up 3 directorys, which should place it on 201701_Capstone
-java -jar ../../../Devel_Dolphins/Build/JAnalysis.jar ${PATH_TO_FILES}
+java -jar ../../../Devel_Dolphins/Build/JAnalysis.jar ${PATH_TO_FILES} > ${timestamp}.txt
