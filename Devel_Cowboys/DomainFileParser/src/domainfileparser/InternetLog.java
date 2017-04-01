c1011c864f729335474f4d3cdacb3ca430423872
webpro_nick <nawoschiknicholas@gmail.com>
2017-04-01 11:04:40 -0500

    Updated the Internet Log class of the Cowboy's tool to be able to parse the offical Internet Log. Also ran myBuild and updated the current build files.

--- Devel_Cowboys/DomainFileParser/src/domainfileparser/InternetLog.java
+++ Devel_Cowboys/DomainFileParser/src/domainfileparser/InternetLog.java
@@ -5,6 +5,9 @@
  */
 package domainfileparser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Nick Nawoschik
@@ -16,6 +19,34 @@ public class InternetLog {
     
     public static String[] parse(String[] lines) {
         
-        return lines;
+        // Checks if data is in the file
+        if(lines.length < 1) {
+            return new String[] {"Error", "Parse Error: No lines were received."};
+        }
+        
+        // Prepares an array to store parsed domains
+        List<String> lineList = new ArrayList<>();
+        
+        // Parses the domains getting rid of blank lines and the header
+        // information (contained up to line 29).
+        for(int i = 0; i < lines.length; i++) {
+            if(i > 29 && !lines[i].equals("")) {
+                String[] line = lines[i].split(" ");
+                
+                for (String value : line) {
+                    if (value.contains("(")) {
+                        if(!value.equals("(0)")) {
+                            String tmp = value;
+                            tmp = tmp.replaceAll("\\(\\d+\\)", ".");
+                            tmp = tmp.substring(1);
+                            tmp = tmp.substring(0, tmp.length()-1);
+                            lineList.add(tmp);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return lineList.toArray(new String[0]); 
     }
 }
